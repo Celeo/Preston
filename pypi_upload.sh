235b832f21d5cd743582609dc74a8070702673ac
Celeo <celeodor@gmail.com>
2016-07-02 21:00:51 -0700

    Update PyPI upload script

--- pypi_upload.sh
+++ pypi_upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-pandoc --from=markdown --to=rst --output=README.rst README.md
-python setup.py sdist upload -r pypi
-rm README.rst
+pandoc --from=markdown --to=rst --output=README.rst README.md || exit 1
+python setup.py sdist upload -r pypi || exit 1
+rm README.rst || exit 1
 echo 'Done'
